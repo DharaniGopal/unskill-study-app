68c60d723ea473c8caf7569f180ca72629f46d68
Dharani Gopal <dharanigopal100897@gmail.com>
2026-06-26 14:55:09 +0530

    [US]-callback-sueref-commit

--- upskill-study-app/src/App.jsx
+++ upskill-study-app/src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 // import Counter from "./pages/Counter";
-import Event from "./pages/Event";
+// import Event from "./pages/Event";
+// import UseRefCounter from "./pages/useRefCounter";
+import UseCallbackCounter from "./pages/UseCallbacKCounter";
 
 function App() {
   return (
@@ -9,7 +11,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           {/* <Route path={"/"} element={<Counter />} /> */}
-          <Route path={"/"} element={<Event />} />
+          {/* <Route path={"/"} element={<Event />} /> */}
+          {/* <Route path={"/"} element={<UseRefCounter />} /> */}
+          <Route path={"/"} element={<UseCallbackCounter />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ upskill-study-app/src/pages/useRefCounter.jsx
@@ -0,0 +1,22 @@
+import React,{useRef} from 'react';
+
+function UseRefCounter(){
+    const [count,setCount] = useState(0);
+
+    const previousCount = useRef();
+
+    useEffect(() => {
+        previousCount.current = count;
+    },[count])
+    
+    return(
+        <>
+        <h1>UseRefCounter</h1>
+        <h1>count: {count}</h1>
+        <h1>PreviousCount:{previousCount.current}</h1>
+        <button onClick={() => setCount(prev => prev + 1)}>Increment</button>
+        </>
+    )
+}
+
+export default UseRefCounter;
